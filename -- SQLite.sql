194fca3e7d7a600f5e7e615f92e25bf94ef00f0f
Manilopez <viratmani619@gmail.com>
2023-02-21 19:35:44 +0530

    End of Section 2

---
+++ -- SQLite.sql
@@ -0,0 +1,12 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Mani");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, "Torture");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Dina");
+
+INSERT INTO Users (Id, UserName)
+VALUES (4, "Tommy");
